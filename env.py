b2bd419f36dee50aa6de0f4335cb0512b3948044
kentqian <kentqian94@gmail.com>
2018-03-27 19:04:10 -0400

    first version

---
+++ env.py
@@ -0,0 +1,103 @@
+from __future__ import print_function
+from collections import defaultdict
+from itertools import count
+import numpy as np
+import math
+import random
+
+class Environment(object):
+    """
+    The Tic-Tac-Toe Environment
+    """
+    # possible ways to win
+    win_set = frozenset([(0,1,2), (3,4,5), (6,7,8), # horizontal
+                         (0,3,6), (1,4,7), (2,5,8), # vertical
+                         (0,4,8), (2,4,6)])         # diagonal
+    # statuses
+    STATUS_VALID_MOVE = 'valid'
+    STATUS_INVALID_MOVE = 'inv'
+    STATUS_WIN = 'win'
+    STATUS_TIE = 'tie'
+    STATUS_LOSE = 'lose'
+    STATUS_DONE = 'done'
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        """Reset the game to an empty board."""
+        self.grid = np.array([0] * 9) # grid
+        self.turn = 1                 # whose turn it is
+        self.done = False             # whether game is done
+        return self.grid
+
+    def render(self):
+        """Print what is on the board."""
+        map = {0:'.', 1:'x', 2:'o'} # grid label vs how to plot
+        print(''.join(map[i] for i in self.grid[0:3]))
+        print(''.join(map[i] for i in self.grid[3:6]))
+        print(''.join(map[i] for i in self.grid[6:9]))
+        print('====')
+
+    def check_win(self):
+        """Check if someone has won the game."""
+        for pos in self.win_set:
+            s = set([self.grid[p] for p in pos])
+            if len(s) == 1 and (0 not in s):
+                return True
+        return False
+
+    def step(self, action):
+        """Mark a point on position action."""
+        assert type(action) == int and action >= 0 and action < 9
+        # done = already finished the game
+        if self.done:
+            return self.grid, self.STATUS_DONE, self.done
+        # action already have something on it
+        if self.grid[action] != 0:
+            return self.grid, self.STATUS_INVALID_MOVE, self.done
+        # play move
+        self.grid[action] = self.turn
+        if self.turn == 1:
+            self.turn = 2
+        else:
+            self.turn = 1
+        # check win
+        if self.check_win():
+            self.done = True
+            return self.grid, self.STATUS_WIN, self.done
+        # check tie
+        if all([p != 0 for p in self.grid]):
+            self.done = True
+            return self.grid, self.STATUS_TIE, self.done
+        return self.grid, self.STATUS_VALID_MOVE, self.done
+
+    def random_step(self):
+        """Choose a random, unoccupied move on the board to play."""
+        pos = [i for i in range(9) if self.grid[i] == 0]
+        move = random.choice(pos)
+        return self.step(move)
+
+    def play_against_random(self, action):
+        """Play a move, and then have a random agent play the next move."""
+        state, status, done = self.step(action)
+        if not done and self.turn == 2:
+            state, s2, done = self.random_step()
+            if done:
+                if s2 == self.STATUS_WIN:
+                    status = self.STATUS_LOSE
+                elif s2 == self.STATUS_TIE:
+                    status = self.STATUS_TIE
+                else:
+                    raise ValueError("???")
+        return state, status, done
+
+
+env = Environment()
+for i in range(0,9):
+    a, b, c = env.random_step()
+    print (a,b,c)
+    if env.check_win():
+        break
+
+env.render()
